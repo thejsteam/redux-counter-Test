8e1222f2dedf554457f6034eced1e35ddb36453a
thejsteam <justinsamuelbusiness@gmail.com>
2023-10-07 12:40:54 -0400

    React-Redux Test

---
+++ src/components/Counter.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Counter = ({ count, increment, decrement }) => {
+  return (
+    <div>
+      <h1>Counter: {count} </h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: "INCREMENT" }),
+    decrement: () => dispatch({ type: "DECREMENT" }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
